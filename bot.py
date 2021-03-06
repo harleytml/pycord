d5283305aab4b27060ce5fb2dc2afc30b78c6c89
harley <harleyworthington240@gmail.com>
2021-03-06 09:38:42 +0000

    accidental token, reset, no issue, thanks discord

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import src.pycord
 
-bot = pycord.Bot("ODE3NDg3ODYzNjc4NjMyMDA3.YEKO5g.WXg_ImIpwuMylYDA2EPuiVFRCSo")
+bot = pycord.Bot("YOUR-TOKEN-HERE")
 
 bot.sendMessage("MESSAGE_CONTENT_HERE", CHANNELIDHERE)
